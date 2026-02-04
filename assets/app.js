8dc87844f0a5ae59e45f8c49216786fc4d34753e
Yohann <at.yohann@gmail.com>
2026-02-04 09:30:31 +0100

    [NEW] add sass + page home

--- assets/app.js
+++ assets/app.js
@@ -1,11 +1,11 @@
-import { createRoot } from 'react-dom/client';
+// import { createRoot } from 'react-dom/client';
 
 // Clear the existing HTML content
 document.body.innerHTML = '<div id="app"></div>';
 
 // Render your React component instead
-const root = createRoot(document.getElementById('app'));
-root.render(<h1>Hello, world</h1>);
+// const root = createRoot(document.getElementById('app'));
+// root.render(<h1>Hello, world</h1>);
 
 /*
  * Welcome to your app's main JavaScript file!
@@ -13,6 +13,6 @@ root.render(<h1>Hello, world</h1>);
  * This file will be included onto the page via the importmap() Twig function,
  * which should already be in your base.html.twig.
  */
-import './styles/app.css';
+import './styles/app.scss';
 
 console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
